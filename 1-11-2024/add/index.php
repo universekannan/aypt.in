b167d20e5cd0e3820ce6a9ac96e9afb6a5e963fd
universekannan <universekannan@gmail.com>
2024-11-01 14:46:03 +0530

    Kannan PC

---
+++ 1-11-2024/add/index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Arulmigu Yoga Parameswarar Temple</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+	<meta name="viewport" content="width = 1050, user-scalable = no" />
+	<script type="text/javascript" src="extras/jquery.min.1.7.js"></script>
+    <script type="text/javascript" src="extras/modernizr.2.5.3.min.js"></script>
+</head>
+<body>
+
+<div class="flipbook-viewport">
+	<div class="container">
+		<div class="flipbook">
+			<div style="background-image:url(pages/1.jpg)"></div>
+			<div style="background-image:url(pages/2.jpg)"></div>
+			<div style="background-image:url(pages/3.jpg)"></div>
+			<div style="background-image:url(pages/4.jpg)"></div>
+			<div style="background-image:url(pages/5.jpg)"></div>
+			<div style="background-image:url(pages/6.jpg)"></div>
+			<div style="background-image:url(pages/7.jpg)"></div>
+			<div style="background-image:url(pages/8.jpg)"></div>
+			<div style="background-image:url(pages/9.jpg)"></div>
+			<div style="background-image:url(pages/10.jpg)"></div>
+			<div style="background-image:url(pages/11.jpg)"></div>
+			<div style="background-image:url(pages/12.jpg)"></div>
+			<div style="background-image:url(pages/13.jpg)"></div>
+			<div style="background-image:url(pages/14.jpg)"></div>
+			<div style="background-image:url(pages/15.jpg)"></div>
+			<div style="background-image:url(pages/16.jpg)"></div>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+
+function loadApp() {
+
+	// Create the flipbook
+
+	$('.flipbook').turn({
+			// Width
+
+			width:922,
+			
+			// Height
+
+			height:600,
+
+			// Elevation
+
+			elevation: 50,
+			
+			// Enable gradients
+
+			gradients: true,
+			
+			// Auto center this flipbook
+
+			autoCenter: true
+
+	});
+}
+
+// Load the HTML4 version if there's not CSS transform
+
+yepnope({
+	test : Modernizr.csstransforms,
+	yep: ['lib/turn.js'],
+	nope: ['lib/turn.html4.min.js'],
+	both: ['css/basic.css'],
+	complete: loadApp
+});
+
+</script>
+
+</body>
+</html>
--- 1-11-2024/menu.php
+++ 1-11-2024/menu.php
@@ -19,19 +19,17 @@ font-size:16px;
                 </div>
                 <div class="collapse navbar-collapse menuContainer" id="myNavbarbar">
                     <ul class="nav navbar-nav mn">
-                        <li class="topMeuHome hidden-xs"><a href="index.php"></a></li>
-                        <li><a href="index.php" class="hidden-sm hidden-md hidden-lg">Home</a></li>
-                        <li class=""><a href="about.php" target=""><span>About us</span></a></li>
-                        <li class=""><a href="pradosha.php" target=""><span>Pradosha</span></a></li>
-  <li class=""><a href="livestream.php" target=""><span>Live</span></a></li>
-						
-						<li class="hidden-md hidden-sm"><a href="calendar.php" target=""><span>Calendar</span></a></li>
+                        <li class="topMeuHome hidden-xs"><a href="index.html"></a></li>
+                        <li><a href="index.html" class="hidden-sm hidden-md hidden-lg">Home</a></li>
+                        <li class=""><a href="about.html" target=""><span>About us</span></a></li>
+                        <li class=""><a href="pradosha.html" target=""><span>Pradosha</span></a></li>						
+						<li class="hidden-md hidden-sm"><a href="calendar.html" target=""><span>Calendar</span></a></li>
 						<li class=""><a href="http://arulmiguyogaparameswarar.blogspot.in/" target=""><span>Blog</span> </a></li>
-                        <li class=""><a href="pothu-marai.php" target=""><span>Pothu Marai</span></a></li>
-                        <li class=""><a href="tamil-thirumarai.php" target=""><span>Tamil maraihal</span></a></li>
-						<li class=""><a href="association.php" target=""><span>Association</span> </a></li>
-						<li class=""><a href="members.php" target=""><span>Members</span> </a></li>
-                        <li class=""><a href="contactus.php" target=""><span>Contact us</span></a></li>
+                        <li class=""><a href="pothu-marai.html" target=""><span>Pothu Marai</span></a></li>
+                        <li class=""><a href="tamil-thirumarai.html" target=""><span>Tamil maraihal</span></a></li>
+						<li class=""><a href="association.html" target=""><span>Association</span> </a></li>
+						<li class=""><a href="members.html" target=""><span>Members</span> </a></li>
+                        <li class=""><a href="contactus.html" target=""><span>Contact us</span></a></li>
                         
                     </ul>
                 </div>
